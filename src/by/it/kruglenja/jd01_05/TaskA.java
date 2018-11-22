91994db09900b53cd251101560bdd92227522067
user <e_rak@icloud.com>
2018-11-22 12:24:36 +0300

    lesson 2-5

--- src/by/it/kruglenja/jd01_05/TaskA.java
+++ src/by/it/kruglenja/jd01_05/TaskA.java
@@ -6,8 +6,9 @@ package by.it.kruglenja.jd01_05;
 public class TaskA {
     public static void main(String[] args) {
         mathA();
+        mathB();
     }
-    static void mathA(){
+    private static void mathA(){
         double res1;
         double a = 756.13;
         double x = 0.3;
@@ -18,11 +19,25 @@ public class TaskA {
 
         res1 = q - w - e;
 
-        double res2 = Math.cos(Math.pow((Math.pow(x, 2) + Math.PI / 6), 5)) - Math.sqrt(x * Math.pow(a, 3)) -
+        double res2 = Math.cos(Math.pow((Math.pow(x, 2) + Math.PI / 6), 5)) - Math.sqrt(x * Math.pow(a, 3))
                 - Math.log(Math.abs((a - 1.12 * x) / 4));
         System.out.println(res1 + " " + res2);
     }
-    static void mathB(){
+    private static void mathB(){
+        double res1;
+        double a = 1.21;
+        double b = 0.371;
 
+        double q = Math.tan(Math.pow(a + b, 2));
+        double w = Math.pow((a + 1.5), 1.0/3);
+        double r = a * Math.pow(b, 5);
+        double e = b / (Math.log(Math.pow(a, 2)));
+
+        res1 = q - w + r - e;
+        System.out.println(res1);
     }
+     private static  void mathC(){
+         double x = 12.1;
+         
+     }
 }
