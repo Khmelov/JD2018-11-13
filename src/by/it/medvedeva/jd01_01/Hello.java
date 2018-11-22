2f0f2a146e4b6c69614d10c5aa1d09ed065ec150
Annaela <annascantech@gmail.com>
2018-11-22 11:56:14 +0300

    lesson 1-4

---
+++ src/by/it/medvedeva/jd01_01/Hello.java
@@ -0,0 +1,16 @@
+package by.it.medvedeva.jd01_01;
+
+public class Hello {
+
+    private String slogan;
+
+    public String getSlogan() {
+        return slogan;
+    }
+
+    public void setSlogan(String slogan) {
+        this.slogan = slogan;
+
+
+    }
+}
