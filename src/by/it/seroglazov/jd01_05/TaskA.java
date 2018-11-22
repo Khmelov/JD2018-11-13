e7b85728435efb74f16b725b6b950c4be682f137
SpaiR <23c84vn9>
2018-11-22 12:19:51 +0300

    Lesson 5 - 1

---
+++ src/by/it/seroglazov/jd01_05/TaskA.java
@@ -0,0 +1,15 @@
+package by.it.seroglazov.jd01_05;
+
+import static java.lang.Math.*;
+
+public class TaskA {
+    public static void main(String[] args) {
+        double a = 756.13;
+        double x =0.3;
+        double part1 = cos(pow(x*x+PI/6, 5));
+        double part2 = sqrt(x*pow(a, 3));
+        double part3 = log((a - 1.12*x) / 4);
+        double z = part1 - part2 - part3;
+        System.out.println(z);
+    }
+}
