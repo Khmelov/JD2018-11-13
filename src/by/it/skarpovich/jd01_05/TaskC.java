7bf715ca6f84743022f6e0f58626c69e856799c6
s-karpovich <mdcatalog2@gmail.com>
2018-11-23 03:10:54 +0300

    Home2 commit

--- src/by/it/skarpovich/jd01_05/TaskC.java
+++ src/by/it/skarpovich/jd01_05/TaskC.java
@@ -2,27 +2,66 @@ package by.it.skarpovich.jd01_05;
 
 import java.util.Random;
 
+import static java.lang.Math.pow;
+
 class TaskC {
-
     public static void main(String[] args) {
+        task1();
+    }
 
-        final int min1 = 20;
-        final int max1 = 40;
-        genLength(min1, max1);
-        //printLength(n);
-        //printLength(int n);
-        // printLength(n);
-//        System.out.println(n);
-//        System.out.println(n); //Как можно вызвать вывод из main?
+    static void task1() {
+        int minArrayLength = 20;
+        int maxArrayLength = 40;
+        double minArrayElement = 5.33;
+        double maxArrayElement = 9.0;
+        double sum = 0;
+        int counter = 0;
+        int counter2 = 0;
+        final int columns = 5;
+        double avg;
+
+        Random rnd1 = new Random();
+//        int n = minArrayLength + rnd1.nextInt(maxArrayLength + 1 - minArrayLength);
+//        //System.out.println("Длина массива: " + n);
+        int n = 28;
+        //Создаем массив
+        double[] arrayA = new double[n];
+        for (int i = 0; i < n; i++) {
+            double x = minArrayElement + Math.random() * (maxArrayElement + 1 - minArrayElement);
+            double z = pow(x * x + 4.5, 1.0 / 3);
+            arrayA[i] = z;
+        }
+        //Выводим массив в 5 колонок.
+
+        //int counter = 0;
+        System.out.println("Массив A[]");
+        for (int i = 0; i < arrayA.length; i++) {
+            System.out.printf("%2s[% -3d]=%-10g", "A", i, arrayA[i]);
+            counter++;
+            if ((counter % columns == 0) || (arrayA.length - 1 == i)) {
+                System.out.println();
+            }
+
+        }
+
+        System.out.println("Массив B[] из элементов массива A > 3.5");
+        for (int i = 0; i < arrayA.length; i++) {
+            if (arrayA[i] > 3.5) {
+
+                System.out.printf("%2s[% -3d]=%-10g", "B", i, arrayA[i]);
+                counter2++;
+                if ((counter2 % columns == 0) || (arrayA.length - 1 == i)) {
+                    System.out.println();
+                }
+                sum += arrayA[i];
+            }
+
+        }
+        avg = pow(sum, 1.0 / counter2);
+        System.out.println();
+        System.out.println("Среднее геометрическое массив B[]: " + avg);
+
     }
-    static int genLength(int min, int max) {
-        int n = 0;
-        Random random = new Random();
-        n = min + random.nextInt(max + 1);
-        System.out.println(n);
-        return n;
-    }
-    static void printLength(int n) {
-        System.out.println(n); //тут разобрался, работает.
-    }
+
 }
+
