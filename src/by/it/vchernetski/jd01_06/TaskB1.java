b42d8502537a6d3c43fc82e747060242673eec9b
VadimChernetski <Vadim.Chernetski1991@gmail.com>
2018-11-24 23:12:24 +0300

    jd01_06 A1,A2

---
+++ src/by/it/vchernetski/jd01_06/TaskB1.java
@@ -0,0 +1,26 @@
+package by.it.vchernetski.jd01_06;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskB1 {
+//    private static String[] words = new String[1];
+//    public static void main(String[] args) {
+//        StringBuilder sb = new StringBuilder(Poem.text);
+//        Pattern pt = Pattern.compile("[бвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЩ][а-я]*[аеёиоуэюя][ ]");
+//        Matcher m = pt.matcher(Poem.text);
+//        while(m.find()){
+//            String[] temp = new String[words.length+1];
+//            for (int i = 0; i < words.length; i++) {
+//                temp[i] = words[i];
+//                String s = m.group();
+//                String x = s.trim();
+//                temp[temp.length-1] = s;
+//            }
+//            words = temp;
+//        }
+//        for (int i = 0; i < words.length; i++) {
+//            System.out.println(words[i]);
+//        }
+//    }
+}
