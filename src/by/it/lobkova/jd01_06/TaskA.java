5162c9b89687b85e459645ea4cfe2da57af27362
Dasha <dashabertosh1958@gmail.com>
2018-11-24 15:32:42 +0300

    ok

---
+++ src/by/it/lobkova/jd01_06/TaskA.java
@@ -0,0 +1,18 @@
+package by.it.lobkova.jd01_06;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskA {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder(Poem.text);
+        Pattern p = Pattern.compile("[а-яА-яёЁ]{4,}");
+        Matcher m = p.matcher(Poem.text);
+        while (m.find()){
+            sb.setCharAt(m.start(), '#');
+          //  if (m.group().length()>= 7) sb.setCharAt((pos + 6), '#');
+        }
+        System.out.println(sb.toString());
+    }
+}
