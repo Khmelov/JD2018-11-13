e060a07d3a72ab14bda8e25418837659e746b89f
Катя <katia.denisova.99@mail.ru>
2018-11-26 19:36:00 +0300

    Solved Lessons 2-4

---
+++ src/by/it/denisova/jd01_04/TaskA.java
@@ -0,0 +1,55 @@
+package by.it.denisova.jd01_04;
+
+import java.util.Scanner;
+
+public class TaskA {
+
+    public static void main(String[] args) {
+       printMulTable();
+        Scanner scanner = new Scanner(System.in);
+        buildOneDimArray(scanner.nextLine());
+    }
+
+
+
+    static void printMulTable(){
+        for (int i = 2; i <10 ; i++) {
+            for (int j = 2; j <10 ; j++) {
+                System.out.printf("%1d*%1d=%-2d ", i,j,i*j);
+            }
+            System.out.println();
+        }
+    }
+
+    static void buildOneDimArray(String line){
+
+        double[] array = InOut.getArray(line);
+        InOut.printArray(array,"V", 50);
+        double first = array[0];
+        double last = array[array.length-1];
+        Helper.sort(array);
+        InOut.printArray(array, "V", 4);
+
+        for (int i = 0; i < array.length; i++) {
+            if( array[i] ==first){
+                System.out.printf("Index of first element=%d\n",i);
+                break;
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if( array[i] ==last){
+                System.out.printf("Index of last element=%d\n",i);
+                break;
+            }
+        }
+
+        /*for (int i = 0; i <array.length ; i++) {
+            if(array[i]==first){
+                System.out.printf("Index of first element=%d\n",i);
+            break;}
+
+        }*/
+
+    }
+}
