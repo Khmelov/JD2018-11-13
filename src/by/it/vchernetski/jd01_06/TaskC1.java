c8d2b92c14aeafdc4a74fe557b1e0e9eb4083489
VadimChernetski <Vadim.Chernetski1991@gmail.com>
2018-11-26 21:50:58 +0300

    c1????

--- src/by/it/vchernetski/jd01_06/TaskC1.java
+++ src/by/it/vchernetski/jd01_06/TaskC1.java
@@ -1,51 +1,34 @@
 package by.it.vchernetski.jd01_06;
 
+import javax.print.DocFlavor;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskC1 {
-    private static StringBuilder[] getmas(){
-        StringBuilder sb=new StringBuilder(Poem.text);
-        StringBuilder[] mas = new StringBuilder[0];
-        Matcher m = Pattern.compile(".+\\n").matcher(Poem.text);
-        int i=0;
-        while(m.find()){
-            StringBuilder[] temp = new StringBuilder[mas.length+1];
-            System.arraycopy(mas,0,temp,0,mas.length);
-            temp[mas.length] = new StringBuilder(m.group().trim());
-            mas = temp;
-            i=m.end();
-        }
-        String str = sb.substring(i);
-        StringBuilder[] masfin = new StringBuilder[mas.length+1];
-        System.arraycopy(mas,0,masfin,0,mas.length);
-        masfin[mas.length] = new StringBuilder(str);
-        return masfin;
-    }
-    private static  void redmas(StringBuilder[] mas){
-        int maxln=0; //максимальная длина
-        for (int i = 0; i < mas.length; i++) {
-            if(mas[i].length()>maxln) maxln=mas[i].length();
-        }
-        for (int i = 0; i < mas.length; i++) {
-            while(true){
-                Matcher m = Pattern.compile("[А-яё|\\p{Punct}] ").matcher(mas[i]);
-                int x = mas[i].length();
-                while(m.find()){
-                    if(mas[i].length()==maxln)
-                        break;
-                    mas[i].insert(m.end()," ");
-                }
-                if(mas[i].length()==maxln)
-                    break;
-            }
-            if(i!=mas.length-1) mas[i].append("\n");
-        }
-    }
     public static void main(String[] args) {
-        StringBuilder[] mymas = getmas();
-        redmas(mymas);
-        StringBuilder[] mymas1 = new StringBuilder[mymas.length];
+        String[] lines = Poem.text.split("\n");
+        StringBuilder[] lb = new StringBuilder[lines.length];
+        for (int i = 0; i < lines.length; i++) {
+            lb[i] = new StringBuilder(lines[i]);
+        }
+        int maxlen = 0;
+        for (int i = 0; i <lines.length; i++) {
+            if(lines[i].length()>maxlen) maxlen = lines[i].length();
+        }
+        for (int i = 0; i < lb.length; i++) {
+            while(true){
+                Matcher m = Pattern.compile("[А-яё|\\p{Punct}] ").matcher(lb[i]);
+                if(lb[i].length() == maxlen) break;
+                while(m.find()){
+                    if(lb[i].length() == maxlen) break;
+                    lb[i].insert(m.end(),' ');
+                }
+            }
+        }
+        for (int i = 0; i < lb.length-1; i++) {
+            lb[i].append("\n");
+        }
+        StringBuilder[] mymas1 = new StringBuilder[lb.length];
         mymas1[0] = new StringBuilder("У     лукоморья     дуб     зелёный,\n");
         mymas1[1] = new StringBuilder("Златая    цепь    на    дубе    том.\n");
         mymas1[2] = new StringBuilder("И    днём   и   ночью   кот   учёный\n");
@@ -79,15 +62,18 @@ public class TaskC1 {
         mymas1[30] = new StringBuilder("У    моря    видел    дуб   зелёный,\n");
         mymas1[31] = new StringBuilder("Под   ним   сидел,   и   кот  учёный\n");
         mymas1[32] = new StringBuilder("Свои     мне     сказки     говорил.");
-//        for (int i = 0; i < mymas1.length; i++) {
-//            if(mymas[i].equals(mymas1[i])){
-//                System.out.print(mymas[i]);
-//                System.out.print(mymas1[i]);
-//            }
-//        }
-
-        for (int i = 0; i < mymas.length; i++) {
-            System.out.print(mymas1[i].toString());
+        String [] mas1 = new String[lb.length];
+        String [] mas2 = new String[mymas1.length];
+        for (int i = 0; i < lb.length; i++) {
+            mas1[i]=lb[i].toString();
+            mas2[i]=mymas1[i].toString();
         }
+        for (int i = 0; i < mas1.length; i++) {
+            if(mas2[i].equals(mas1[i]))
+            System.out.println(i+"y");
+            else System.out.println(i+"NOOOOOOOOOO");
+        }
+        System.out.println(mas1[29]);
+        System.out.println(mas1[32]);
     }
 }
