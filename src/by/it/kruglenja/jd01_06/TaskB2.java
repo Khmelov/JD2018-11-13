108951fabbfc76de466d09447b6e21513b7ef092
Sargarous <sergey.kruglenja@gmail.com>
2018-11-26 21:34:30 +0300

    TaskA2B1

---
+++ src/by/it/kruglenja/jd01_06/TaskB2.java
@@ -0,0 +1,13 @@
+package by.it.kruglenja.jd01_06;
+
+import java.util.regex.Pattern;
+
+public class TaskB2 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder(Poem.text);
+        Pattern pattern = Pattern.compile("[,...]");
+    }
+    private static void clearer(){
+
+    }
+}
