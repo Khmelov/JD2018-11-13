14ea4f4f40dbb63e82d3826b0c42cfa5c5777c88
Sargarous <sergey.kruglenja@gmail.com>
2018-11-27 20:32:29 +0300

    Merge branch 'master' of https://github.com/Sargarous/JD2018-11-13
    # Conflicts:
    #	src/by/it/kruglenja/jd01_06/TaskB2.java

--- src/by/it/kruglenja/jd01_06/TaskB2.java
+++ src/by/it/kruglenja/jd01_06/TaskB2.java
@@ -13,43 +13,31 @@ public class TaskB2 {
         while (matcher.find()) {
             len++;
         }
-        int[] counter = new int[len];
         matcher.reset();
-        int c = 0;
+        String[] lines = new String[len];
+        int count = 0;
         while (matcher.find()) {
-            counter[c] = matcher.end() - 1 - matcher.start();
-            System.out.println(counter[c]);
-            c++;
+            lines[count] = matcher.group();
+            count++;
         }
-        int[] printOrder = new int[len];
-        System.arraycopy(counter, 0, printOrder, 0, printOrder.length);
+        matcher.reset();
         boolean flag = false;
+
         while (!flag) {
             flag = true;
-            for (int i = 0; i < printOrder.length - 1; i++) {
-                int temp = 0;
-                if (printOrder[i] > printOrder[i + 1]) {
-                    temp = printOrder[i];
-                    printOrder[i] = printOrder[i + 1];
-                    printOrder[i + 1] = temp;
+            for (int i = 0; i < lines.length - 1; i++) {
+                String temp = " ";
+                if (lines[i].length() > lines[i + 1].length()) {
+                    temp = lines[i];
+                    lines[i] = lines[i + 1];
+                    lines[i + 1] = temp;
                     flag = false;
                 }
             }
-
-            }System.out.println("_________________________________________");
-        for (int aPrintOrder : printOrder) {
-            System.out.println(aPrintOrder);
         }
-//        Pattern pattern = Pattern.compile("[^а-яА-ЯёЁ]*");
-//        Matcher matcher = pattern.matcher(sb);
-//        while (matcher.find()){
-//            System.out.println(matcher.group());
-//
-//            int x = matcher.start();
-//            System.out.println(sb.charAt(x));
-//            sb.deleteCharAt(x);
-//
-//        }
-//        System.out.println(sb.toString());
+        StringBuilder sortedLines = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            System.out.print(lines[i]);
+        }
     }
 }
