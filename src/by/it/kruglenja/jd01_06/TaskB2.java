0f75ad4887ea20540753589599efb9f70406acbb
user <e_rak@icloud.com>
2018-11-27 11:33:02 +0300

    Merge branch 'master' of https://github.com/Sargarous/JD2018-11-14
    # Conflicts:
    #	src/by/it/kruglenja/jd01_06/TaskA1.java
    #	src/by/it/kruglenja/jd01_06/TaskA2.java

--- src/by/it/kruglenja/jd01_06/TaskB2.java
+++ src/by/it/kruglenja/jd01_06/TaskB2.java
@@ -13,32 +13,28 @@ public class TaskB2 {
         while (matcher.find()) {
             len++;
         }
-        int[] counter = new int[len];
-        matcher.reset();
-        int c = 0;
+        String[]newArr = new String[len];        
+        int count = 0;
         while (matcher.find()) {
-            counter[c] = matcher.end() - 1 - matcher.start();
-            System.out.println(counter[c]);
-            c++;
+            newArr[count] = matcher.group();
+            count++;
         }
-        int[] printOrder = new int[len];
-        System.arraycopy(counter, 0, printOrder, 0, printOrder.length);
         boolean flag = false;
         while (!flag) {
             flag = true;
-            for (int i = 0; i < printOrder.length - 1; i++) {
-                int temp = 0;
-                if (printOrder[i] > printOrder[i + 1]) {
-                    temp = printOrder[i];
-                    printOrder[i] = printOrder[i + 1];
-                    printOrder[i + 1] = temp;
+            for (int i = 0; i < newArr.length - 1; i++) {
+                String temp = "";
+                if (newArr[i].length() > newArr[i + 1].length()) {
+                    temp = newArr[i];
+                    newArr[i] = newArr[i + 1];
+                    newArr[i + 1] = temp;
                     flag = false;
                 }
             }
 
             }System.out.println("_________________________________________");
-        for (int aPrintOrder : printOrder) {
-            System.out.println(aPrintOrder);
+        for (String anewArr : newArr) {
+            System.out.println(anewArr);
         }
 //        Pattern pattern = Pattern.compile("[^а-яА-ЯёЁ]*");
 //        Matcher matcher = pattern.matcher(sb);
