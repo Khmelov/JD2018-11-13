2ea6af4494fa583abfc5634c52ed9c71eff142f3
LeonidEvenchik <aeroforus@gmail.com>
2018-11-29 00:25:01 +0300

    2

--- src/by/it/evenchik/jd01_06/TaskA2.java
+++ src/by/it/evenchik/jd01_06/TaskA2.java
@@ -8,45 +8,41 @@ import java.util.regex.Pattern;
 
 public class TaskA2 {
 
-     private static String[] w ={};
-     private static int[] count={};
+    private static String[] w = {}; //masiv strok//
+    private static int[] count = {};//masiv intov//
 
+
+
+    ////zakinuli slovo iz matchera(odni slovo iz metoda groupp)
     private static int pos(String word){
-        for (int i = 0; i <w.length ; i++) {
-            if (w[i].equals(word));
-                return i ;
-
+        for (int i = 0; i < w.length; i++) { //perebiraem stroki dlya priverki na povtori
+            if(w[i].equals(word))            //sravnivaem soderjimoe masiva strok "w" po indeksu "i" s slovom na vhode metoda "pos"
+            return i;                        //vernulo index povtornogo elementa.
         }
         return -1;
     }
-
-
-    public static void main(String [] args){
-        Pattern pattern =  Pattern.compile( "[а-яА-ЯёЁ]+");
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
         Matcher mathcer = pattern.matcher(Poem.text);
-        while (mathcer.find()){
+        while(mathcer.find()){
             String word = mathcer.group();
-            int p =pos(word);
-            if (p >= 0) {
+            int p = pos(word);
+            if(p >= 0){
                 count[p]++;
+
             }
             else{
-                int last=w.length;
-                w = Arrays.copyOf(w,last+1);
-                w[last]=word;
-                count= Arrays.copyOf(count,last+1);
-                count[last]=1;
+                int last = w.length; //izmerili masiv i prisvoili dlinnu v intovuju peremenuyu last
+                w = Arrays.copyOf(w, last + 1);  // skopirovsli  masiv iz starogo i uvelichili na odin element (stariy w ushel pod garbage collector)
+                w[last] = word; //  zapisali slovo v mas "W" v index "last"
+                count = Arrays.copyOf(count, last + 1);
+                count[last] = 1;    /// soderjanie masiva so slovovom bez povtoreniy.
+
             }
         }
-        for (int i = 0; i <w.length ; i++) {
-            System.out.println(w[i]+"="+count[i]);
-
+        for (int i = 0; i < w.length; i++) {
+            System.out.println(w[i] + "=" + count[i]);
         }
-}
 
-
-
-
-
-
+    }
 }
