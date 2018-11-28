fa7b31ac6df47a60ac8c097e9635624e6da40bf9
maksimbaidak <maksimbaidk@gmail.com>
2018-11-28 13:20:45 +0300

    homework lesson 1 - 7

--- src/by/it/baidak/jd01_06/TaskB1.java
+++ src/by/it/baidak/jd01_06/TaskB1.java
@@ -1,88 +1,52 @@
 package by.it.baidak.jd01_06;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskB1 {
 
-    private static String[] words = new String[0];
-    private static int[] counts = new int[0];
-
-
     public static void main(String[] args) {
 
         Pattern p = Pattern.compile("[а-яА-яёЁ]{2,}");
         Matcher m = p.matcher(Poem.text);
-        String[] mas = new String[300];
-        int counter = 0;
+        List<String> list = new ArrayList<>();
         while (m.find()) {
-            String word = m.group();
-            process(word);
+            list.add(m.group());
         }
-        for (int i = 0; i < words.length; i++) {
-            if(checkLetters(words[i])) System.out.print(words[i] + " ");
-        }
-    }
-
-    private static boolean checkLetters(String word) {
-        if (checkCons(word.toUpperCase()) && checkVow(word.toUpperCase())) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private static boolean checkCons(String word) {
-        switch (word.charAt(0)){
-            case 'Б':
-            case 'В':
-            case 'Г':
-            case 'Ж':
-            case 'З':
-            case 'К':
-            case 'М':
-            case 'Н':
-            case 'П':
-            case 'Р':
-            case 'С':
-            case 'Ф':
-            case 'Х':
-            case 'Ц':
-            case 'Ч':
-            case 'Ш':
-            case 'Щ': return true;
-            default: return false;
-        }
-    }
-
-    private static boolean checkVow (String word){
-        switch (word.charAt(word.length() - 1)) {
-            case 'А':
-            case 'Е':
-            case 'Ё':
-            case 'И':
-            case 'О':
-            case 'У':
-            case 'Ы':
-            case 'Э':
-            case 'Ю':
-            case 'Я': return true;
-            default: return false;
-        }
-    }
-
-    private static void process(String word) {
-        for (int i = 0; i < words.length; i++) {
-            if (word.equals(words[i])) {
-                counts[i]++;
-                return;
+        for (String word : list) {
+            if(!checkVow(word.charAt(0))){
+                if (checkVow(word.charAt(word.length() - 1))){
+                    System.out.println(word);
+                }
             }
         }
-        String[] words2 = new String[words.length + 1];
-        int[] counts2 = new int[counts.length + 1];
-        System.arraycopy(words,0,words2,0,words.length);
-        System.arraycopy(counts,0,counts2,0,counts.length);
-        words=words2; words[words.length-1]=word;
-        counts=counts2; counts[counts.length-1]=1;
+    }
+
+    private static boolean checkVow (char c){
+        switch (c) {
+            case 'А':
+            case 'а':
+            case 'Е':
+            case 'е':
+            case 'Ё':
+            case 'ё':
+            case 'И':
+            case 'и':
+            case 'О':
+            case 'о':
+            case 'У':
+            case 'у':
+            case 'Ы':
+            case 'ы':
+            case 'Э':
+            case 'э':
+            case 'Ю':
+            case 'ю':
+            case 'Я':
+            case 'я':return true;
+            default: return false;
+        }
     }
 }
--- src/by/it/baidak/jd01_06/TaskB2.java
+++
@@ -1,8 +0,0 @@
-package by.it.baidak.jd01_06;
-
-public class TaskB2 {
-
-    public static void main(String[] args) {
-        
-    }
-}
