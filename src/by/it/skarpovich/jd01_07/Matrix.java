c42ef41cde2a6d451419efa3dfbbb0c13fd7af0a
s-karpovich <mdcatalog2@gmail.com>
2018-11-29 01:09:58 +0300

    JD 07

--- src/by/it/skarpovich/jd01_07/Matrix.java
+++ src/by/it/skarpovich/jd01_07/Matrix.java
@@ -13,57 +13,31 @@ class Matrix extends Var {
                 this.value = value;
             }
         }
-        //System.arraycopy(value, 0, this.value, 0, this.value.length);
     }
 
     Matrix(Matrix matrix) {
         this.value = matrix.value;
     }
-//
-//    Matrix(String strMatrix) {
-//
-//        Matcher matcher = Pattern.compile("\\{(.*)\\}").matcher(strMatrix);
-//
-//        while (matcher.find()) {
-//            String[][] strMatrixArray = matcher.group(1).replace(" ", "").split(",");
-//
-//            value = new double[strMatrixArray1.length][strMatrixArray2.length]
-//            for (int i = 0; i <strMatrixArray1.length; i++) {
-//                for (int j = 0; j < strMatrixArray2.length; j++) {
-//                    value[i][j]=
-//
-//                }
-//            }
-//
-//
-//            double[] y = new double[strMatrixArray2.length];
-//            for (int i = 0; i <strMatrixArray2.length; i++) {
-//                y[i] = Double.parseDouble(strMatrixArray2[i]);
-//            }
-//
-//
-//        }
-//    }
 
-//        while (matcher.find()) {
-//            String[] strMatrixArray = matcher.group().replace(" ", "").split(",");
-//            value = new double[strMatrixArray.length][strMatrixArray.length];
-//            for (int i = 0; i < strMatrixArray.length; i++) {
-//                for (int j = 0; j < strMatrixArray.length; j++) {
-//                    value[i][j] = Double.parseDouble(strMatrixArray[i]);
-//                    System.out.println(value[i][j]);
-//                }
-//            }
-//        }
+    Matrix(String strMatrix) {
 
+        Matcher matcher = Pattern.compile("\\{(.*)\\}").matcher(strMatrix);
+        while (matcher.find()) {
+            String[] strMatrixArray = matcher.group().replace(" ", "").split(",");
+            value = new double[strMatrixArray.length][strMatrixArray.length];
+            for (int i = 0; i < strMatrixArray.length; i++) {
+                for (int j = 0; j <strMatrixArray.length; j++) {
+                    value[i][j] = Double.parseDouble(strMatrixArray[i]);
+                }
+            }
+        }
+    }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("{");
-        String delimiter = "";
         int row = 0;
-        int i = 0;
-        int j = 0;
+        int i, j;
         for (i = 0; i < 2; i++) {
             sb.append("{");
             for (j = 0; j < 2; j++) {
@@ -80,11 +54,6 @@ class Matrix extends Var {
         }
         sb.append("}");
         return sb.toString();
-
-
-//        String v3=Arrays.deepToString(value);
-//        return v3;
-
     }
 }
 
