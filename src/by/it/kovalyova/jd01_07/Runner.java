a44e78dd43fde728585d90ff13ed04a130a80f31
kovalyovaolga <kovalyovaolga1980@gmail.com>
2018-11-28 21:44:48 +0300

    01_07_A1-A3

---
+++ src/by/it/kovalyova/jd01_07/Runner.java
@@ -0,0 +1,12 @@
+package by.it.kovalyova.jd01_07;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        Var v1=new Scalar(3.14);
+        Var v2=new Vector();
+
+        System.out.println(v1);
+        System.out.println(v2);
+    }
+}
---
+++ src/by/it/kovalyova/jd01_07/Vector.java
@@ -0,0 +1,4 @@
+package by.it.kovalyova.jd01_07;
+
+class Vector extends Var {
+}
