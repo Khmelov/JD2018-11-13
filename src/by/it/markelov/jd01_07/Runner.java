e1712da62d459e6b14d4ca2f8a925428bd575dbc
MarkelaLippi <MarkelaLippi@gmail.com>
2018-11-29 18:53:26 +0300

    jd01_07 С 80%

--- src/by/it/markelov/jd01_07/Runner.java
+++ src/by/it/markelov/jd01_07/Runner.java
@@ -2,15 +2,49 @@ package by.it.markelov.jd01_07;
 
 public class Runner {
     public static void main(String[] args) {
-        Var v1=new Scalar(0.12345);
-        Var v2=new Scalar((Scalar) v1);
-        Var v3=new Scalar("0.12345");
+        String[] s = "{{1.0, 2.0}, {3.0, 4.0}}".split("}, ");
 
-        System.out.println(v1);
-        System.out.println(v2);
-        System.out.println(v3);
+        for (String s1 : s) {
+            //System.out.println(s1);
 
-       // double v []={1,2,3};
-        v1=new Vector(new double[]{1,2,3});
+        }
+        for (int i = 0; i < s.length; i++) {
+            s[i] = s[i].replaceAll("[,}{]", "");}
+
+        for (String s1 : s) {
+                //System.out.println(s1);
+
+            }
+
+
+        String splitstrings1 []=new String[2];
+        splitstrings1=s[0].split(" ");
+
+        String splitstrings2 []=new String[2];
+        splitstrings2=s[1].split(" ");
+
+        double mas1[]=new double[splitstrings1.length];
+        double mas2[]=new double[splitstrings2.length];
+
+        for (int i = 0; i < splitstrings1.length; i++) {
+            mas1[i]=Double.parseDouble(splitstrings1[i]);
+            mas2[i]=Double.parseDouble(splitstrings2[i]);
+        }
+        for (double v : mas1) {
+            System.out.print(v+" ");
+
+        }
+        System.out.println();
+
+        for (double v : mas2) {
+            System.out.print(v+" ");
+
+        }
+
+
+
+        }
+
     }
-}
+
+
--- src/by/it/markelov/jd01_07/Vector.java
+++ src/by/it/markelov/jd01_07/Vector.java
@@ -1,28 +1,26 @@
 package by.it.markelov.jd01_07;
 
-import java.util.Arrays;
-
 class Vector extends Var {
     double value[];
 
     public Vector(double[] value) {
-        this.value = value;
-        //System.arraycopy(this.value,0,value,0,value.length);
+        this.value = new double[value.length];
+        System.arraycopy(value, 0, this.value, 0, value.length);
     }
 
     public Vector(Vector vector) {
-        value = vector.value;
-        //System.arraycopy(this.value,0,vector.value,0,value.length);
+        this.value = new double[vector.value.length];
+        System.arraycopy(vector.value, 0, this.value, 0, vector.value.length);
     }
 
     public Vector(String strVector) {
-        String withoutPunct = strVector.replace("[,{}]", "");
-        String[] s = withoutPunct.split(" ");
-        double temp[] = new double[s.length];
+        String withoutBraces = strVector.replaceAll("[}{]", "");
+        String[] s = withoutBraces.split(",");
+        value = new double[s.length];
         for (int i = 0; i < s.length; i++) {
-            temp[i] = Double.parseDouble(s[i]);
+            value[i] = Double.parseDouble(s[i]);
         }
-        value = temp;
+
     }
 
     @Override
