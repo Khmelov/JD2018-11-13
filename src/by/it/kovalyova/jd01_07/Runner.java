a98a4d496722d782e486f3ba2ed78ed68a181d15
kovalyovaolga <kovalyovaolga1980@gmail.com>
2018-11-29 08:15:03 +0300

    JD01_06_B1, JD01_07_B1-B3

--- src/by/it/kovalyova/jd01_07/Runner.java
+++ src/by/it/kovalyova/jd01_07/Runner.java
@@ -4,7 +4,8 @@ public class Runner {
     public static void main(String[] args) {
 
         Var v1=new Scalar(3.14);
-        Var v2=new Vector();
+        double[] t = {1.0,2.0,4.0};
+        Var v2=new Vector(t);
 
         System.out.println(v1);
         System.out.println(v2);
