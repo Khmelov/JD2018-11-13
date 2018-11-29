77b8975be0374c8b712896cdf05f396584d70f3e
maksimbaidak <maksimbaidk@gmail.com>
2018-11-29 14:37:24 +0300

    homework lesson 8 all B tasks work

--- src/by/it/baidak/jd01_08/Operation.java
+++ src/by/it/baidak/jd01_08/Operation.java
@@ -3,7 +3,7 @@ package by.it.baidak.jd01_08;
 /**
  * Created by user on 29.11.2018.
  */
-public interface Operation {
+interface Operation {
     Var add(Var v);
     Var sub(Var v);
     Var mul(Var v);
--- src/by/it/baidak/jd01_08/Var.java
+++ src/by/it/baidak/jd01_08/Var.java
@@ -3,29 +3,34 @@ package by.it.baidak.jd01_08;
 /**
  * Created by user on 27.11.2018.
  */
-public class Var implements Operation {
+abstract class Var implements Operation {
 
     @Override
-    public Var add(Var v) {
+    public Var add(Var other) {
+        System.out.println("Сложение "+this+"+"+other+" невозможно!");
         return null;
     }
 
     @Override
-    public Var sub(Var v) {
+    public Var sub(Var other) {
+        System.out.println("Вычитание "+this+"-"+other+" невозможно!");
         return null;
     }
 
     @Override
-    public Var mul(Var v) {
+    public Var mul(Var other) {
+        System.out.println("Умножение "+this+"*"+other+" невозможно!");
         return null;
     }
 
     @Override
-    public Var div(Var v) {
+    public Var div(Var other) {
+        System.out.println("Деление "+this+"/"+other+" невозможно!");
         return null;
     }
 
-    public Var createVar(String exp){
-        return null;
+    @Override
+    public String toString() {
+        return "this Var";
     }
 }
