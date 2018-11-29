7cb0c4f000b285701ee5360827f30ff731dad8ed
YuriyBerdnik <yuriy8671@gmail.com>
2018-11-29 20:54:38 +0300

    -8 a,b

---
+++ src/by/it/berdnik/jd01_08/Vector.java
@@ -0,0 +1,81 @@
+package by.it.berdnik.jd01_08;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Vector extends Var {
+
+    private double[] value;
+
+    Vector(double[] value) {
+        this.value = Arrays.copyOf(value, value.length);
+    }
+
+    Vector(String strVector) {
+        Matcher matcher = Pattern.compile("[\\w]").matcher(strVector);
+        int str = 0;
+        while (matcher.find()) {
+            str++;
+        }
+        value = new double[str];
+        str = 0;
+        matcher.reset();
+        while (matcher.find()) {
+            value[str++] = Double.parseDouble(matcher.group());
+        }
+    }
+
+
+    Vector(Vector vector) {
+        this.value = vector.value;
+    }
+
+
+    @Override
+    public Var add(Var other) {
+        if (other instanceof Scalar) {
+            double[] add = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < add.length; i++) {
+                add[i] = add[i] + ((Scalar) other).getValue();
+            }
+            return new Vector(add);
+        } else if (other instanceof Vector) {
+            double[] add = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < add.length; i++) {
+                add[i] = add[i] + ((Vector) other).value[i];
+            }
+            return new Vector(add);
+        } else return super.add(other);
+    }
+
+    @Override
+    public Var sub(Var other) {
+        if (other instanceof Scalar) {
+            double[] sub = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < sub.length; i++) {
+                sub[i] = sub[i] - ((Scalar) other).getValue();
+            }
+            return new Vector(sub);
+        } else if (other instanceof Vector) {
+            double[] sub = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < sub.length; i++) {
+                sub[i] = sub[i] - ((Vector) other).value[i];
+            }
+            return new Vector(sub);
+        } else return super.sub(other);
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("{");
+        String delimiter = "";
+        for (double element : value) {
+            sb.append(delimiter).append(element);
+            delimiter = ", ";
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+}
