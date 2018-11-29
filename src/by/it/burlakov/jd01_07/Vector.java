aba0ad20f3afdcfbcc6fa2d29cd09c46cd9f3050
burlakov_oleg <diocomm@gmail.com>
2018-11-29 09:42:03 +0300

    Merge branch 'master' of https://github.com/diologStorage/JD2018-11-13
    # Conflicts:
    #	src/by/it/burlakov/jd01_07/Runner.java
    #	src/by/it/burlakov/jd01_07/Vector.java

--- src/by/it/burlakov/jd01_07/Vector.java
+++ src/by/it/burlakov/jd01_07/Vector.java
@@ -7,49 +7,40 @@ import java.util.Arrays;
  */
 public class Vector extends Var {
 
-<<<<<<< b36ad934598f84161d78637e307b7a61dbcf9efd
     private double[] value;
 
-    Vector(double[] valueVector){
+    Vector(double[] valueVector) {
         this.value = new double[valueVector.length];
-       System.arraycopy(valueVector, 0, this.value, 0, this.value.length);
+        System.arraycopy(valueVector, 0, this.value, 0, this.value.length);
     }
 
-    Vector(Vector otherVector){
+    Vector(Vector otherVector) {
         this.value = new double[otherVector.value.length];
 
         System.arraycopy(otherVector.value, 0, this.value, 0, this.value.length);
     }
 
-    Vector(String value){
+    Vector(String value) {
         value = value.replaceAll("[{|}]", "");
         String[] stringValue = value.split(",");
         this.value = new double[stringValue.length];
         for (int i = 0; i < stringValue.length; i++) {
             this.value[i] = Double.parseDouble(stringValue[i].trim());
         }
-=======
-    private double[] valueVector;
-
-    Vector(double[] valueVector){
-        this.valueVector = new double[valueVector.length];
-       System.arraycopy(valueVector, 0, this.valueVector, 0, this.valueVector.length);
->>>>>>> TaskA
     }
-
-    @Override
     public String toString() {
-<<<<<<< b36ad934598f84161d78637e307b7a61dbcf9efd
-            StringBuilder sb = new StringBuilder("{");
-            String separ = "";
+
+        StringBuilder sb = new StringBuilder("{");
+        String separ = "";
         for (double val : value) {
             sb.append(separ).append(val);
             separ = ", ";
         }
         sb.append("}");
+
         return sb.toString();
-=======
-        return Arrays.toString(valueVector);
->>>>>>> TaskA
+
+
+
     }
 }
