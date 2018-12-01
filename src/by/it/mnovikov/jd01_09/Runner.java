0044cbc016a614d539b393886e314d3c52e48990
zzz <zz88@tut.by>
2018-12-01 14:53:09 +0300

    Lesson_09

---
+++ src/by/it/mnovikov/jd01_09/Runner.java
@@ -0,0 +1,12 @@
+package by.it.mnovikov.jd01_09;
+
+import javax.xml.bind.ParseConversionEvent;
+
+/**
+ * Created by user on 01.12.2018.
+ */
+public class Runner {
+    Parcer p = new Parcer();
+
+
+}
