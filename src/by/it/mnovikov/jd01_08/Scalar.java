b95f2be9c7225fbd4a38644226bad66b70a92032
n00bsya <n00bsya@mail.ru>
2018-12-01 09:39:21 +0300

    08

--- src/by/it/mnovikov/jd01_08/Scalar.java
+++ src/by/it/mnovikov/jd01_08/Scalar.java
@@ -10,14 +10,45 @@ class Scalar extends Var {
         this.value = value;
     }
 
-    Scalar(String strValue){
+    Scalar(String strValue) {
         value = Double.parseDouble(strValue);
     }
 
-    Scalar(Scalar otherScalar){
+    Scalar(Scalar otherScalar) {
         this.value = otherScalar.value;
     }
 
+    @Override
+    public Var add(Var other) {
+        if (other instanceof Scalar) {
+            double sum = this.value + ((Scalar) other).value;
+            return new Scalar(sum);
+        } else return other.add(this);
+    }
+
+    @Override
+    public Var sub(Var other) {
+        if (other instanceof Scalar) {
+            double sub = this.value - ((Scalar) other).value;
+            return new Scalar(sub);
+        } else return new Scalar(-1).mul(other).add(this);
+    }
+
+    @Override
+    public Var mul(Var other) {
+        if (other instanceof Scalar) {
+            double mul = this.value * ((Scalar) other).value;
+            return new Scalar(mul);
+        } else return other.mul(this);
+    }
+
+    @Override
+    public Var div(Var other) {
+        if (other instanceof Scalar) {
+            double div = this.value / ((Scalar) other).value;
+            return new Scalar(div);
+        } else return other.div(this);
+    }
     @Override
     public String toString() {
         return String.valueOf(value);
