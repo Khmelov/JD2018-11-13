1e66063427c1de7e11c2c57f1088d4ba5f9ae564
zzz <zz88@tut.by>
2018-12-01 14:17:22 +0300

    Lesson_08 TaskA

---
+++ src/by/it/mnovikov/jd01_09/Var.java
@@ -0,0 +1,39 @@
+package by.it.mnovikov.jd01_09;
+
+/**
+ * Created by user on 27.11.2018.
+ */
+abstract class Var implements Operation {
+    @Override
+    public Var add(Var other) {
+        System.out.println("Операция сложения " + this + "+" + other + " невозможна");
+        return null;
+    }
+
+    @Override
+    public Var sub(Var other) {
+        System.out.println("Операция вычитания " + this + "-" + other + " невозможна");
+        return null;
+    }
+
+    @Override
+    public Var mul(Var other) {
+        System.out.println("Операция умножения " + this + "*" + other + " невозможна");
+        return null;
+    }
+
+    @Override
+    public Var div(Var other) {
+        System.out.println("Операция деления " + this + "/" + other + " невозможна");
+        return null;
+    }
+
+    public Var() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "this Var";
+    }
+}
