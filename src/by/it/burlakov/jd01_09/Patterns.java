96cec423cab3767f15916b7e9ce083d89eafc535
diologStorage <diocomm@gmail.com>
2018-12-02 18:58:51 +0300

    Matrix09 complit

--- src/by/it/burlakov/jd01_09/Patterns.java
+++ src/by/it/burlakov/jd01_09/Patterns.java
@@ -3,8 +3,14 @@ package by.it.burlakov.jd01_09;
 /**
  * Created by user on 01.12.2018.
  */
-public interface Patterns {
-    String OPERATOR = "[-+*/]";
-    String SCALAR = "-?[0-9]+(.[0-9]+)?";
-    String VECTOR = "\\{" + SCALAR + ",?}";
+
+abstract class Patterns {
+    private Patterns(){
+
+    }
+    static final String OPERATOR = "[-+*/]";
+    static final String SCALAR = "-?[0-9]+(.[0-9]+)?";
+    static final String VECTOR = "\\{((-?[0-9]+(.[0-9]+)?)(,?))+}";
+    static final String MATRIX = "\\{(\\{((-?[0-9]+(.[0-9]+)?)(,?))+},?)+}";
 }
+
--- src/by/it/burlakov/jd01_09/Printer.java
+++ src/by/it/burlakov/jd01_09/Printer.java
@@ -4,7 +4,7 @@ package by.it.burlakov.jd01_09;
  * Created by user on 01.12.2018.
  */
 class Printer {
-    public void printer (Var string){
+    public void print (Var string){
         System.out.println(string);
     }
 }
