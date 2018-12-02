1ee91bc62b4a6370c6f2cbe8f49f5b480dfe37b1
diologStorage <diocomm@gmail.com>
2018-12-02 18:58:51 +0300

    Matrix09 complit

---
+++ src/by/it/burlakov/jd01_09/ConsoleRunner.java
@@ -0,0 +1,17 @@
+package by.it.burlakov.jd01_09;
+
+import java.util.Scanner;
+
+
+public class ConsoleRunner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line;
+        Parser parser = new Parser();
+        Printer printer = new Printer();
+        while (!(line = scanner.nextLine()).equals("END")){
+            Var var = parser.calc(line);
+            printer.print(var);
+        }
+    }
+}
