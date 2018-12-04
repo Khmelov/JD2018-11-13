6cb515505274bcbb5e87d3fb8a5ab627b53dccb3
diologStorage <diocomm@gmail.com>
2018-12-04 07:25:24 +0300

    Matrix complit2

--- src/by/it/burlakov/jd01_09/ConsoleRunner.java
+++ src/by/it/burlakov/jd01_09/ConsoleRunner.java
@@ -9,7 +9,7 @@ public class ConsoleRunner {
         String line;
         Parser parser = new Parser();
         Printer printer = new Printer();
-        while (!(line = scanner.nextLine()).equals("END")){
+        while (!(line = scanner.nextLine().toLowerCase()).equals("end")){
             Var var = parser.calc(line);
             printer.print(var);
         }
