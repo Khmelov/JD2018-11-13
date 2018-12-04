00cc408d76ebf4bcef78a0ccaa1e05e45c430f36
Dasha <dashabertosh1958@gmail.com>
2018-12-04 12:10:19 +0300

    Task09-10Start

--- src/by/it/lobkova/jd01_10/PrintMath.java
+++ src/by/it/lobkova/jd01_10/PrintMath.java
@@ -1,4 +1,21 @@
 package by.it.lobkova.jd01_10;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 public class PrintMath {
+
+    public static void main(String[] args) {
+        Class<Math> mathClass = Math.class;
+
+        Method[] methods = mathClass.getDeclaredMethods();
+        for (Method method : methods) {
+            StringBuilder methName = new StringBuilder();
+            int modifiers = method.getModifiers();
+            if ((Modifier.PUBLIC & modifiers) == Modifier.PUBLIC) methName.append("public ");
+            methName.append(method.getName());
+
+        }
+
+    }
 }
