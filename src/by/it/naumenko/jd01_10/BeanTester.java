d290f4cd8ef0aba072b3f497c7d796709b2829af
Сергей Николаевич <naumenkosergeyns@gmail.com>
2018-12-06 02:33:59 +0300

    les 10 С complited

--- src/by/it/naumenko/jd01_10/BeanTester.java
+++ src/by/it/naumenko/jd01_10/BeanTester.java
@@ -1,8 +1,20 @@
 package by.it.naumenko.jd01_10;
 
-public class BeanTester {
-    public static void main(String[] args) throws Exception {
-        
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
+public class BeanTester {
+
+    public static void main(String[] args) throws Exception {
+        Class bean = Bean.class;
+        Bean object = (Bean) bean.newInstance();//создание объекта
+        Method[] declaredMethods = bean.getDeclaredMethods(); //все методы класса Bean
+        for (Method declaredMethod : declaredMethods) {
+            if (declaredMethod.isAnnotationPresent(Param.class)) { //проверка анатирован ли метод?
+                Param annatation = declaredMethod.getAnnotation(Param.class);//Записать аннотацию
+                double rezultat = (double) declaredMethod.invoke(object, annatation.a(), annatation.b());
+                System.out.println(annatation + " " + declaredMethod.getName() + " " + rezultat);
+            }
+        }
     }
 }
