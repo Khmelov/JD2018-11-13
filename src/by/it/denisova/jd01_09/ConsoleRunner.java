adbfce306d8693abff1e36731670da6ec80ceeaa
Катя <katia.denisova.99@mail.ru>
2018-12-07 23:36:07 +0300

    solved 9 a-b

--- src/by/it/denisova/jd01_09/ConsoleRunner.java
+++ src/by/it/denisova/jd01_09/ConsoleRunner.java
@@ -7,13 +7,11 @@ public class ConsoleRunner {
     public static void main(String[] args) {
         Parcer pars = new Parcer();
         Printer printer = new Printer();
-        Scanner scanner= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         String line;
 
 
-
-
-        while (!(line = scanner.next()).equals("END")){
+        while (!(line = scanner.nextLine()).equals("END")){
             Var result = pars.calc(line);
             printer.print(result);
         }
