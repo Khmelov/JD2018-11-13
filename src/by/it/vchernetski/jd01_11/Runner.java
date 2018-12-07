f811a5d43ac53b5f7946c8b754635240f0f6722d
Vadim.Chernetski <Vadim.Chernetski1991@gmail.com>
2018-12-07 23:13:38 +0300

    11ab

--- src/by/it/vchernetski/jd01_11/Runner.java
+++ src/by/it/vchernetski/jd01_11/Runner.java
@@ -5,20 +5,17 @@ import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
-        List<String> myList = new ListA<>();
+        List<String> myList = new ListB<>();
         List<String> arList = new ArrayList<>();
-        myList.add("one");
-        myList.add("two");
-        myList.add("four");
         arList.add("one");
         arList.add("two");
         arList.add("four");
-        System.out.println("myList"+myList);
-        System.out.println("arList"+arList);
-        myList.add("three");
-        arList.add("three");
-        myList.remove("one");
+        myList.add("one");
+        myList.add("two");
+        myList.add("four");
         System.out.println(myList);
-        System.out.println(myList.get(1));
+        System.out.println(arList);
+        myList.addAll(arList);
+        System.out.println(myList);
     }
 }
