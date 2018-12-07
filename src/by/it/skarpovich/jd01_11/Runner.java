fa2c5e2efdfb9d69630aa0e3e1aa770f41d152ce
s-karpovich <mdcatalog2@gmail.com>
2018-12-07 21:56:24 +0300

    JD 11

---
+++ src/by/it/skarpovich/jd01_11/Runner.java
@@ -0,0 +1,28 @@
+package by.it.skarpovich.jd01_11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        List<String> myList = new ListA<>();
+        List<String> arList = new ArrayList<>();
+
+        myList.add("First");
+        arList.add("First");
+        myList.add("Two");
+        arList.add("Two");
+        myList.add("Three");
+        arList.add("Four");
+        System.out.println("add myList:"+myList+"\narList:"+arList);
+        myList.add(2, "Tree"); arList.add(2, "Tree");
+        myList.add(0, "Start"); arList.add(0, "Start");
+        System.out.println("add() myList:"+myList+"\narList:"+arList);
+
+        myList.remove("Start"); arList.remove("Start");
+        myList.remove(3);arList.remove(3);
+        System.out.println("remove myList"+myList+"\narList:"+arList);
+        System.out.println("get myList(0):"+myList.get(0)+"\narList(0):"+arList.get(0));
+
+    }
+}
