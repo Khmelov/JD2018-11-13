8bd8ae220c744ba805ffb6403a2f018893971447
Leon <Aeroforus@gmail.com>
2018-12-08 14:06:19 +0300

    Merge branch 'master' of https://github.com/Khmelov/JD2018-11-13
    # Conflicts:
    #	src/by/it/mnovikov/jd01_09/Runner.java

--- src/by/it/evenchik/jd01_09/Patterns.java
+++ src/by/it/evenchik/jd01_09/Patterns.java
@@ -6,9 +6,9 @@ final class Patterns {
 
     }
 
-    static final String OPERATION="[-+*/]";
+    static final String  OPERATION="[-+*/]";
     static final String SCALAR="-?[0-9]+(.[0-9]+)?";
     static final String VECTOR="\\{((-?[0-9]+(.[0-9]+)?)(,?))+}";
-    static final String MATRIX="\\{(\\{((-?[0-9]+(.[0-9]+)?)(,?))+},?)+}";
+     static final String MATRIX="\\{(\\{((-?[0-9]+(.[0-9]+)?)(,?))+},?)+}";
 
 }
