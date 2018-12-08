333beaa9d670a61356a4e3f84729edef3b8854e2
VladaZaharova <vlada20032002@mail.ru>
2018-12-08 15:02:47 +0300

    class lection 12

---
+++ src/by/it/baidak/jd01_12/TaskA1.java
@@ -0,0 +1,32 @@
+package by.it.baidak.jd01_12;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by user on 08.12.2018.
+ */
+public class TaskA1 {
+
+    private ArrayList<Integer> grades = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        TaskA1 currentClass = new TaskA1();
+        currentClass.clearBad(currentClass.grades);
+        for(int i = 0; i <10;i++){
+            currentClass.grades.add(1 + (int) (Math.random() * 10));
+        }
+        System.out.println(currentClass.grades);
+        currentClass.clearBad(currentClass.grades);
+        System.out.println(currentClass.grades);
+    }
+
+    public void clearBad(ArrayList<Integer> grades) {
+        Iterator<Integer> iterator = grades.iterator();
+        while (iterator.hasNext()) {
+            Integer next = iterator.next();
+            if (next < 4) iterator.remove();
+        }
+    }
+}
