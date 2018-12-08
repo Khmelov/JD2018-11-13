833cc8b78e018fc98b777626281eb33f3dde4295
kovalyovaolga <kovalyovaolga1980@gmail.com>
2018-12-08 10:58:15 +0300

    JD11_AB

---
+++ src/by/it/kovalyova/jd01_11/Runner.java
@@ -0,0 +1,16 @@
+package by.it.kovalyova.jd01_11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<>();
+        list.add("One");
+        list.add("333333");
+        System.out.println(list);
+        list.remove(list);
+
+
+    }
+}
