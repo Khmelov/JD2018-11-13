74db24e7473cdbb76e0bd074b3468d33b78d1825
Vadim.Chernetski <Vadim.Chernetski1991@gmail.com>
2018-12-09 21:55:15 +0300

    jd01_12 A-B

---
+++ src/by/it/vchernetski/jd01_12/TaskB1.java
@@ -0,0 +1,37 @@
+package by.it.vchernetski.jd01_12;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskB1 {
+    private static void print (HashMap<String, Integer> map){
+            StringBuilder output = new StringBuilder();
+            for(Map.Entry<String, Integer> m: map.entrySet()){
+                output.append(m.getKey()+"="+m.getValue().toString()+"\n");
+            }
+        System.out.println(output);
+        }
+    public static void main(String[] args) {
+        HashMap<String, Integer> mymap = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
+        String input;
+        while(!((input=sc.next()).equalsIgnoreCase("end"))){
+            Matcher m = Pattern.compile("[A-z]+[']?[a-z]+").matcher(input);
+            m.reset();
+            if(m.find())
+            input = m.group();
+            if(input.equalsIgnoreCase("isn't")) input = "don't";
+            if(mymap.containsKey(input)) {
+                Integer x =mymap.get(input);
+                mymap.remove(input,x);
+                mymap.put(input,++x);
+                continue;
+            }
+            mymap.put(input,1);
+        }
+        print(mymap);
+    }
+}
