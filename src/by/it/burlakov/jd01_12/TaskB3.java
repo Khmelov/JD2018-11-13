95a6b1a2e864e1d964093604af6e022e551ca450
diologStorage <diocomm@gmail.com>
2018-12-09 23:26:37 +0300

    C complit

--- src/by/it/burlakov/jd01_12/TaskB3.java
+++ src/by/it/burlakov/jd01_12/TaskB3.java
@@ -12,10 +12,27 @@ public class TaskB3 {
 
         ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(peoples));
         LinkedList<String> linkedList = new LinkedList<>(Arrays.asList(peoples));
+        LinkedList<String> linkedList2 = new LinkedList<>(Arrays.asList(peoples));
 
-
+        long startArList = System.nanoTime();
         System.out.println(process(arrayList));
+        long endArList = System.nanoTime();
+
+        long startLinList1 = System.nanoTime();
         System.out.println(process(linkedList));
+        long endLinList1 = System.nanoTime();
+
+        long startLinList2 = System.nanoTime();
+        System.out.println(process2(linkedList2));
+        long endLinList2 = System.nanoTime();
+
+        long deltaArList = (endArList - startArList) / 1000;
+        long deltaLinList1 = (endLinList1 - startLinList1) / 1000;
+        long deltaLinList2 = (endLinList2 - startLinList2) / 1000;
+
+        System.out.println("ArrayList time = " + deltaArList);
+        System.out.println("LinkedList1 time = " + deltaLinList1);
+        System.out.println("LinkedList2 time = " + deltaLinList2);
     }
 
     static String process(ArrayList<String> peoples){
@@ -36,6 +53,19 @@ public class TaskB3 {
                 if (i % 2 == 0) { it.remove();}
                 i++;
             }
+        }
+        return  peoples.get(0);
+    }
+
+    //stack
+    static String process2 (LinkedList<String> peoples){
+//        LinkedList<String> copy = new LinkedList<>(peoples);
+
+        String temp;
+        while (peoples.size() > 1) {
+            temp = peoples.pollFirst();
+            peoples.removeFirst();
+            peoples.addLast(temp);
         }
         return  peoples.get(0);
     }
