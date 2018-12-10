669475ebaa3f67c2a852c97b705e0e8b59abc716
Renat Serahlazau <serahlazau@outlook.com>
2018-12-11 00:00:46 +0300

    jd01_12 A-C1

---
+++ src/by/it/seroglazov/jd01_12/TaskC1.java
@@ -0,0 +1,41 @@
+package by.it.seroglazov.jd01_12;
+
+import java.util.*;
+
+public class TaskC1 {
+
+    public static void main(String[] args) {
+        TreeMap<UUID, String> map = new TreeMap<>();
+        Scanner scanner = new Scanner(System.in);
+        UUID id;
+        while (true) {
+            String str = scanner.next();
+            if (str.equals("end")) break;
+            id = UUID.randomUUID();
+            map.put(id, str);
+        }
+        // Выод на консоль
+        for (Map.Entry<UUID, String> item : map.entrySet()) {
+            System.out.println(item.getKey() + " : " + item.getValue());
+        }
+
+        // Удаляем повторения - для этого ложим в новый HashMap в котором меняем местами ключи и значения
+        HashMap<String, UUID> newMap = new HashMap<>(map.size());
+        for (Map.Entry<UUID, String> item : map.entrySet()) {
+            newMap.putIfAbsent(item.getValue(), item.getKey());
+        }
+        // А теперь складываем назад в map
+        map = new TreeMap<UUID, String>();
+        for (Map.Entry<String, UUID> item : newMap.entrySet()) {
+            map.put(item.getValue(), item.getKey());
+        }
+
+        // Выод на консоль
+        System.out.println("После сжатия:");
+        for (Map.Entry<UUID, String> item : map.entrySet()) {
+            System.out.println(item.getKey() + " : " + item.getValue());
+        }
+
+
+    }
+}
