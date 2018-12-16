7f720cf3d22d2d6c88da806aecee64c64ca45cdb
s-karpovich <mdcatalog2@gmail.com>
2018-12-16 21:35:19 +0300

    Task 15

---
+++ src/by/it/skarpovich/jd01_15/TaskA.java
@@ -0,0 +1,42 @@
+package by.it.skarpovich.jd01_15;
+
+import javafx.concurrent.Task;
+
+import java.io.*;
+
+public class TaskA {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String clDir=TaskA.class.getName().replace(TaskA.class.getSimpleName(),"").replace(".", File.separator);
+        String path = System.getProperty("user.dir")+File.separator+"src"+File.separator+clDir;
+        int[][] matrix = new int[6][4];
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 4; j++) {
+                matrix[i][j] = -15 + (int) ((Math.random() * 31));
+            }
+        }
+        try (PrintWriter prw = new PrintWriter(new FileWriter(path+"matrix.txt"))) {
+            for (int i = 0; i < 6; i++) {
+                for (int j = 0; j < 4; j++) {
+                   prw.printf("%3d%s", matrix[i][j]," ");
+                    //System.out.printf("%3d", matrix[i][j]);
+                }
+               // System.out.println();
+                 prw.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+try(BufferedReader bfr=new BufferedReader(new FileReader(path+"matrix.txt"))){
+    StringBuilder sbuilder=new StringBuilder();
+while(bfr.ready()){
+    sbuilder.append(bfr.readLine()).append("\n");
+
+}    System.out.println(sbuilder);
+} catch (IOException e) {
+    e.printStackTrace();
+}
+
+
+    }
+}
