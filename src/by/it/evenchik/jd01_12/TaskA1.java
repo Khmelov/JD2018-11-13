a664a5744619851afe42aa6e650aeb774204f8fa
Leon <Aeroforus@gmail.com>
2018-12-17 17:24:25 +0300

    Merge branch 'master' of https://github.com/Khmelov/JD2018-11-13
    # Conflicts:
    #	src/by/it/mnovikov/jd01_09/Runner.java

--- src/by/it/evenchik/jd01_12/TaskA1.java
+++ src/by/it/evenchik/jd01_12/TaskA1.java
@@ -2,12 +2,24 @@ package by.it.evenchik.jd01_12;
 
 import by.it.nickgrudnitsky.jd01_04.TaskA;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
 
 public class TaskA1 {
 
     private List<Integer>list=new ArrayList<>();
+
+    private void clearBad (List <Integer> grades){
+        Iterator<Integer> iterator = grades.iterator();
+        while (iterator.hasNext()){
+            int grade = iterator.next();
+            if(grade< 4)
+            iterator.remove();
+
+        }
+
+    }
 
 
     public static void main(String[] args) {
@@ -16,6 +28,8 @@ public class TaskA1 {
             task.list.add((int) Math.round(Math.random()*10));
 
         }
-        System.out.println(task);
+        System.out.println(task.list);
+        task.clearBad(task.list);
+        System.out.println(task.list);
     }
 }
