5b6f2b2702875121eaed58788a2eb43e4ca42029
Катя <katia.denisova.99@mail.ru>
2018-12-20 00:55:41 +0300

    Solved 02_01 a-b basket?

---
+++ src/by/it/denisova/jd02_01/Dispatcher.java
@@ -0,0 +1,10 @@
+package by.it.denisova.jd02_01;
+
+public class Dispatcher {
+    static final int SPEED = 10;
+    static int buyerCounter = 0;
+    static int goodCounter = 0;
+    static int backetCount = 30;
+
+
+}
---
+++ src/by/it/denisova/jd02_01/IUseBacket.java
@@ -0,0 +1,6 @@
+package by.it.denisova.jd02_01;
+
+public interface IUseBacket {
+    void takeBacket(); //взял корзину
+    void putGoodsToBacket();
+}
