ad4b1b3fb3f1355191a24d35acfe56380c46c3a8
s-karpovich <mdcatalog2@gmail.com>
2018-12-21 16:50:34 +0300

    JD 02_02-taskA with Basket

--- src/by/it/skarpovich/jd02_02/Cashier.java
+++ src/by/it/skarpovich/jd02_02/Cashier.java
@@ -10,17 +10,17 @@ public class Cashier implements Runnable {
 
     @Override
     public void run() {
-        System.out.println(this+" started work");
+        System.out.println(this+" started the work");
         while (!Dispatcher.marketClosed()){
             Buyer buyer = QueueBuyer.extract();
             if (buyer==null) {
                 Util.sleep(1);
             }
             else {
-                System.out.println(this+"started service "+buyer);
+                System.out.println(this+"started to serve "+buyer);
                 int time=Util.random(2000,5000);
                 Util.sleep(time);
-                System.out.println(this+"finished service "+buyer);
+                System.out.println(this+"served "+buyer);
                 synchronized (buyer){
                     buyer.notify();
                 }
