47349103303d881d41fb4fa1cb024a72dd271d7f
Leon <Aeroforus@gmail.com>
2018-12-30 17:04:28 +0300

    Merge branch 'master' of https://github.com/Khmelov/JD2018-11-13
    # Conflicts:
    #	src/by/it/mnovikov/jd01_09/Runner.java

--- src/by/it/evenchik/jd01_13/TaskA.java
+++ src/by/it/evenchik/jd01_13/TaskA.java
@@ -2,35 +2,30 @@ package by.it.evenchik.jd01_13;
 
 import java.util.HashMap;
 
-
-    public class TaskA {
+    public class  TaskA {
         public static void main(String[] args) {
             try{
-                if (Math.random()>0.5){
-                    new HashMap<String, String>(null);
-                } else {
-                    Integer.parseInt("привет");
-                }
-
-            } catch (NullPointerException | NumberFormatException e){
-                String name = e.getClass().getName();
+            if (Math.random() > 0.5)
+                new HashMap<String, String>(null);
+            else
+                Integer.parseInt("привет");
+            }
+            catch(Exception e){
                 StackTraceElement[] stackTrace = e.getStackTrace();
-                String myClass = by.it.burlakov.jd01_13.TaskA.class.getName();
-
                 for (StackTraceElement element : stackTrace) {
-                    String className = element.getClassName();
-                    if(className.equals(myClass)){
+                    if(TaskA.class.getName().equals(element.getClassName())){
+                        System.out.println(element);
+                        String name = e.getClass().getName();
+                        String clname = element.getClassName();
                         int lineNumber = element.getLineNumber();
-                        System.out.printf(
-                                "name: %s\n" +
+                        int number=element.getLineNumber();
+                        System.out.printf(" name: %s\n" +
                                 "class: %s\n" +
                                 "line: %d\n",
-                                name, className, lineNumber);
+                                name, clname, lineNumber);
                         break;
                     }
                 }
-
-                System.out.println("catched " + name);
             }
 
         }
