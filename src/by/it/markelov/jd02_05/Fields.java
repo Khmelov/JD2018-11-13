3d523e45bac944ba8ff0cdedc15305053a93a436
MarkelaLippi <MarkelaLippi@gmail.com>
2019-01-07 13:24:04 +0300

    jd02_05 A

---
+++ src/by/it/markelov/jd02_05/Fields.java
@@ -0,0 +1,19 @@
+package by.it.markelov.jd02_05;
+
+public class Fields {
+    private static final String WELCOME="welcome";
+    private static final String INTRODUCE="introduce";
+    private static final String NAME="name";
+
+    public static String getWELCOME() {
+        return WELCOME;
+    }
+
+    public static String getINTRODUCE() {
+        return INTRODUCE;
+    }
+
+    public static String getNAME() {
+        return NAME;
+    }
+}
---
+++ src/by/it/markelov/jd02_05/Runner.java
@@ -0,0 +1,11 @@
+package by.it.markelov.jd02_05;
+
+import java.util.ResourceBundle;
+
+public class Runner {
+    public static void main(String[] args) {
+        ResourceBundle resourceBundle;
+
+
+    }
+}
