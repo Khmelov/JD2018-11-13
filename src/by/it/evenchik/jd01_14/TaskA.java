e677b99a450a55e14d8a49fb82a4bd1ef19f1c92
Leon <Aeroforus@gmail.com>
2019-01-08 11:04:21 +0300

    Merge branch 'master' of https://github.com/Khmelov/JD2018-11-13
    # Conflicts:
    #	src/by/it/mnovikov/jd01_09/Runner.java

--- src/by/it/evenchik/jd01_14/TaskA.java
+++ src/by/it/evenchik/jd01_14/TaskA.java
@@ -1,13 +1,18 @@
 package by.it.evenchik.jd01_14;
 import java.io.*;
+import java.util.Scanner;
 
 public class TaskA {
 
 
     private static String dir(Class<?> cl) {
-        String path = System.getProperty("user.dir" + File.separator + "src" + File.separator);
-        String clDir = cl.getName().replace(cl.getSimpleName(), "".replace(".", File.separator));
-        return path;
+        String path = System.getProperty("user.dir") + File.separator + "src" + File.separator;
+
+        System.out.println("PATH----"+path);
+        String clDir = cl.getName().replace(cl.getSimpleName(), "").replace(".", File.separator);
+        System.out.println("clDir--------"+clDir);
+        System.out.println("path + clDir--------- "+path+clDir);
+        return path + clDir;
 
 
     }
@@ -19,9 +24,10 @@ public class TaskA {
                     new BufferedOutputStream(
                             new FileOutputStream(dir(TaskA.class)+"dataTaskA.bin"))) ;
             for (int i = 0; i < 20; i++) {
-                dos.writeInt((int) (Math.random() * 25));
+                dos.writeInt((int) (Math.random() * 20));
 
             }
+            System.out.println("dos------"+dos);
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -43,8 +49,9 @@ public class TaskA {
                 new BufferedInputStream(
                         new FileInputStream(dir(TaskA.class)+"dataTaskA.bin")));
             PrintWriter out2= new PrintWriter(
-                    new FileWriter(dir(TaskA.class)+"resultTaskA.txt ")
-            )
+                    new FileWriter(dir(TaskA.class)+"resultTaskA.txt")
+            );
+
 
 
         ){
@@ -53,16 +60,20 @@ public class TaskA {
             while (inp.available()>0 ) {
                 int i = inp.readInt();
                 System.out.print(i+" ");
-                out2.print( i +"  ");
+                out2.print( i +" ");
                 sum = sum + i;
                 count++;
             }
             System.out.println("\n avg="+sum/count );
             System.out.println("\navg" +sum/count);
+
         }
         catch (IOException e) {
             e.printStackTrace();
         }
 
+
+
     }
+
 }
