f1617c65168debd2e0a7b1d9ac35115ed15e4ed8
kovalyovaolga <kovalyovaolga1980@gmail.com>
2019-01-09 01:16:33 +0300

    jd02_06a

---
+++ src/by/it/kovalyova/jd02_06/Main.java
@@ -0,0 +1,17 @@
+package by.it.kovalyova.jd02_06;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Logger logger = Logger.getInstance();
+        logger.log("One");
+        logger = Logger.getInstance();
+        logger.log("2");
+        logger = Logger.getInstance();
+        logger.log("3");
+        logger = Logger.getInstance();
+        logger.log("4");
+
+
+    }
+}
