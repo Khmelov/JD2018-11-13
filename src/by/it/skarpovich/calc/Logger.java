98e55c12dafa42ca49d70696932c7214b49e317b
s-karpovich <mdcatalog2@gmail.com>
2019-01-09 22:32:42 +0300

    jd02_06 v2

--- src/by/it/skarpovich/calc/Logger.java
+++ src/by/it/skarpovich/calc/Logger.java
@@ -3,6 +3,10 @@ package by.it.skarpovich.calc;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 class Logger {
 
@@ -26,7 +30,13 @@ class Logger {
                 BufferedWriter out = new BufferedWriter(
                         new FileWriter(Util.getPath("log.txt"),true)
                 )) {
-            out.write(text + "\n");
+
+
+
+            Date d = new Date();
+            DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.LONG, ResMan.INSTANCE.getLocale());
+
+            out.write(text + " " + dateFormat.format(d)+ "\n");
             out.flush();
 
         } catch (IOException e1) {
