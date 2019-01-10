8bac101c6f542786edee7356540cfbd06c07b724
kovalyovaolga <kovalyovaolga1980@gmail.com>
2019-01-10 08:27:14 +0300

    jd02_06b

--- src/by/it/kovalyova/jd02_06/Main.java
+++ src/by/it/kovalyova/jd02_06/Main.java
@@ -1,17 +1,22 @@
 package by.it.kovalyova.jd02_06;
 
+
+
+import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
+        try {
+            Logger logger = Logger.getInstance();
+            logger.log("One");
+            logger = Logger.getInstance();
+            logger.log("2");
+            logger = Logger.getInstance();
+            logger.log("3");
+            logger = Logger.getInstance();
+            logger.log("4");
 
-        Logger logger = Logger.getInstance();
-        logger.log("One");
-        logger = Logger.getInstance();
-        logger.log("2");
-        logger = Logger.getInstance();
-        logger.log("3");
-        logger = Logger.getInstance();
-        logger.log("4");
-
-
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
