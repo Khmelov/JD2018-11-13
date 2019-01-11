68ffe4a012dd7d948cb5e6ba21c4ca5d12bf5ce6
Vadim.Chernetski <Vadim.Chernetski1991@gmail.com>
2019-01-11 21:24:33 +0300

    jd02_06 all

--- src/by/it/vchernetski/calcwithpatterns/Patterns.java
+++ src/by/it/vchernetski/calcwithpatterns/Patterns.java
@@ -2,7 +2,7 @@ package by.it.vchernetski.calcwithpatterns;
 
 final class Patterns {
     private  Patterns(){}
-    static final String OPERATION = "(?<![-+*/={,])([-+=*/])";
+    static final String OPERATION = "(?<![-+*/={,]|,\\s|^)([-+=*/])";
     static final String SCALAR = "-?[0-9]+(.[0-9]+)?";
     static final String VECTOR = "\\{(-?([0-9]+)(\\.[0-9]+)*,? ?)+\\}";
     static final String MATRIX = "\\{(\\{(-?([0-9]+)(\\.[0-9]+)*,? ?)+\\}[, ]*)+\\}";
