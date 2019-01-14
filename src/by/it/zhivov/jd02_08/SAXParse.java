4c37c20bc3afd5bb5440ae2ad67cec4f3643d07d
jerome <drel@icloud.com>
2019-01-14 22:54:33 +0300

    jd02_07_complete(modded with <atributes>) jd02_08_start

---
+++ src/by/it/zhivov/jd02_08/SAXParse.java
@@ -0,0 +1,34 @@
+/*
+package by.it.zhivov.jd02_08;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SAXParse extends DefaultHandler {
+
+    private String tab;
+    private  StringBuilder content;
+
+    @Override
+    public void startDocument() throws SAXException {
+    }
+
+    @Override
+    public void endDocument() throws SAXException {
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        new StringBuilder()
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+    }
+}
+*/
