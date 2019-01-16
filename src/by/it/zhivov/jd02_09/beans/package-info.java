145264a5f7a9ecc9405fbb87ffe2a6c63c67ba0d
jerome <drel@icloud.com>
2019-01-17 00:03:10 +0300

    jd_02_07(ABC) jd_02_08(A,B) jd_02_09_(A)

---
+++ src/by/it/zhivov/jd02_09/beans/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://beans.jd02_09.zhivov.it.by/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package by.it.zhivov.jd02_09.beans;
