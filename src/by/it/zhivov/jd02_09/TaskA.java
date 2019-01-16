ed2570835ff39fd34dca16b42de9ec36b570ebac
jerome <drel@icloud.com>
2019-01-17 00:03:10 +0300

    jd_02_07(ABC) jd_02_08(A,B) jd_02_09_(A)

--- src/by/it/zhivov/jd02_09/TaskA.java
+++ src/by/it/zhivov/jd02_09/TaskA.java
@@ -1,7 +1,10 @@
 package by.it.zhivov.jd02_09;
 
 
-import by.it.zhivov.jd02_09.beans.*;
+import by.it.zhivov.jd02_09.beans.Ad;
+import by.it.zhivov.jd02_09.beans.Ads;
+import by.it.zhivov.jd02_09.beans.Persons;
+import by.it.zhivov.jd02_09.beans.User;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -13,6 +16,7 @@ import java.io.FileReader;
 
 public class TaskA {
     public static void main(String[] args) {
+        //String fileXML = System.getProperty("user.dir") + "/src/by/it/zhivov/jd02_07/person+xsd.xml";
         String generatedXML = System.getProperty("user.dir") + "/src/by/it/zhivov/jd02_09/generated.xml";
         generateToXML();
         generateFromXML(generatedXML);
@@ -41,16 +45,13 @@ public class TaskA {
             Marshaller marshaller = jaxbContext.createMarshaller();
             Persons persons = new Persons();
             Ads ads = new Ads();
-            Ad ad = new Ad("Bmwx5", "Nice car", "BMW", "m3", "red", "sedan", 2006, "Comfort", 658, false, 7200);
-            User user = new User("Anton Zhivov", "Jerome94", "A59915991", "23.12.1994", "jerome@astero.com", "+375333830046", ads);
-            persons.getUser().add(user);
+            Ad ad = new Ad("Полное дно", "BMW", "m3", "red", "sedan", 2006, "Comfort", 658, false, 7200);
+            User s = new User("Zhivov", "Jerome", "A59915991", "23.12.1994", "jeromeastero@mail.ru", "+375330000000", false, ads);
+            persons.getUser().add(s);
             ads.getAd().add(ad);
-            ad = new Ad("apeer", "Super 8", "Volvo", "XC90", "red", "sedan", 2006, "Comfort", 658, false, 7200);
-            ads.getAd().add(ad);
-            ads = new Ads();
-            user = new User("Pepper", "guzli", "awdawwa22", "23.12.2016", "dwadaw@dwaWD.ru", "+375333830046", ads);
-            ad = new Ad("Batmobile", "uragan", "VAZ", "2101", "pupule", "sedan", 1985, "Comfort", 580000, true, 23000);
-            persons.getUser().add(user);
+            ad = new Ad("Супер дно", "АУДИ", "m3", "fff", "sedan", 2006, "Comfort", 658, true, 7200);
+            s = new User("FFFF", "JeroWDWme", "A59915991", "23.12.1994", "jeromeasawdawtero@mail.ru", "+375330000000", false, ads);
+            persons.getUser().add(s);
             ads.getAd().add(ad);
             marshaller.marshal(persons, new FileOutputStream("src/by/it/zhivov/jd02_09/generated.xml"));
             System.out.println("Xml generated");
