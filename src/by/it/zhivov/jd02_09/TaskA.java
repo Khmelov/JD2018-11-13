2df83e70b3fb0e56d788a1e2faa4ee013ee10973
jerome <drel@icloud.com>
2019-01-17 00:03:10 +0300

    jd_02_07(ABC) jd_02_08(A,B) jd_02_09_(A)

--- src/by/it/zhivov/jd02_09/TaskA.java
+++ src/by/it/zhivov/jd02_09/TaskA.java
@@ -1,7 +1,10 @@
 package by.it.zhivov.jd02_09;
 
 
-import by.it.zhivov.jd02_09.beans.*;
+import by.it.zhivov.jd02_09.beans.Ad;
+import by.it.zhivov.jd02_09.beans.Ads;
+import by.it.zhivov.jd02_09.beans.Persons;
+import by.it.zhivov.jd02_09.beans.User;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -13,6 +16,7 @@ import java.io.FileReader;
 
 public class TaskA {
     public static void main(String[] args) {
+        //String fileXML = System.getProperty("user.dir") + "/src/by/it/zhivov/jd02_07/person+xsd.xml";
         String generatedXML = System.getProperty("user.dir") + "/src/by/it/zhivov/jd02_09/generated.xml";
         generateToXML();
         generateFromXML(generatedXML);
@@ -42,21 +46,13 @@ public class TaskA {
             Persons persons = new Persons();
             Ads ads = new Ads();
             Ad ad = new Ad("Полное дно", "BMW", "m3", "red", "sedan", 2006, "Comfort", 658, false, 7200);
-            User user = new User("Zhivov", "Jerome", "A59915991", "23.12.1994", "jeromeastero@mail.ru", "+375330000000", false, ads);
-            persons.getUser().add(user);
+            User s = new User("Zhivov", "Jerome", "A59915991", "23.12.1994", "jeromeastero@mail.ru", "+375330000000", false, ads);
+            persons.getUser().add(s);
             ads.getAd().add(ad);
-            ads = new Ads();
             ad = new Ad("Супер дно", "АУДИ", "m3", "fff", "sedan", 2006, "Comfort", 658, true, 7200);
-            user = new User("FFFF", "JeroWDWme", "A59915991", "23.12.1994", "jeromeasawdawtero@mail.ru", "+375330000000", false, ads);
-            persons.getUser().add(user);
+            s = new User("FFFF", "JeroWDWme", "A59915991", "23.12.1994", "jeromeasawdawtero@mail.ru", "+375330000000", false, ads);
+            persons.getUser().add(s);
             ads.getAd().add(ad);
-            ads = new Ads();
-            user = new User("wwww", "JeroWDWme", "A59915991", "23.12.1994", "jeromeasawdawtero@mail.ru", "+375330000000", false, ads);
-            persons.getUser().add(user);
-            Admin admin = new Admin("admin", "JJ2242", true);
-            persons.getAdmin().add(admin);
-            Guest guest = new Guest(false);
-            persons.getGuest().add(guest);
             marshaller.marshal(persons, new FileOutputStream("src/by/it/zhivov/jd02_09/generated.xml"));
             System.out.println("Xml generated");
             marshaller.marshal(persons, System.out);
