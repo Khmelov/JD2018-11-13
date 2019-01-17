299814a7b24ec176645352e87837f2c2ea8b67dc
Leon <Aeroforus@gmail.com>
2019-01-17 09:35:22 +0300

    01

---
+++ src/by/it/evenchik/jd02_06/Main.java
@@ -0,0 +1,14 @@
+package by.it.evenchik.jd02_06;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = Logger.getInstance();
+        logger.log("One");
+        logger = Logger.getInstance();
+        logger.log("two");
+        logger = Logger.getInstance();
+        logger.log("three");
+        logger = Logger.getInstance();
+        logger.log("four");
+    }
+}
