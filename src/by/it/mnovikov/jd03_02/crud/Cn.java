7fdf37aec5f2e8e7efb9fd2e58f5d78608735b6a
zzz <zz88@tut.by>
2019-01-19 15:22:28 +0300

    03_02

---
+++ src/by/it/mnovikov/jd03_02/crud/Cn.java
@@ -0,0 +1,12 @@
+package by.it.mnovikov.jd03_02.crud;
+
+/**
+ * Created by user on 19.01.2019.
+ */
+public interface Cn {
+    String URL = "jdbc:mysql://127.0.0.1:2016/mnovikov" + "?useUnicode=true&characterEncoding=UTF-8";
+
+    String USER = "root";
+
+    String PASSWORD = "";
+}
---
+++ src/by/it/mnovikov/jd03_02/crud/Connect.java
@@ -0,0 +1,33 @@
+package by.it.mnovikov.jd03_02.crud;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by user on 19.01.2019.
+ */
+
+class Connect {
+    private static volatile Connection connection;
+
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static Connection getConnection() throws SQLException {
+        if (connection==null || connection.isClosed()){
+            synchronized (Connect.class) {
+                if (connection==null || connection.isClosed()){
+                    connection= DriverManager.getConnection(Cn.URL, Cn.USER, Cn.PASSWORD);
+                }
+            }
+        }
+        return connection;
+    }
+
+}
