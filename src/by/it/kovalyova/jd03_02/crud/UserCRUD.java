ed6479e85192ec81aeea93b81a9d3f596601cb60
kovalyovaolga <kovalyovaolga1980@gmail.com>
2019-01-20 10:54:23 +0300

    jd03_02

--- src/by/it/kovalyova/jd03_02/crud/UserCRUD.java
+++ src/by/it/kovalyova/jd03_02/crud/UserCRUD.java
@@ -15,8 +15,8 @@ class UserCRUD {
                 user.getLogin(), user.getPassword(), user.getEmail(), user.getRoles_Id()
         );
         try (Connection connection = Connect.getConnection();
-             Statement statement = connection.createStatement()){
-            if (1== statement.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS)) {
+             Statement statement = connection.createStatement()) {
+            if (1 == statement.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS)) {
                 ResultSet generatedKeys = statement.getGeneratedKeys();
                 if (generatedKeys.next()) {
                     user.setId(generatedKeys.getLong(1));
@@ -26,6 +26,7 @@ class UserCRUD {
         }
         return false;
     }
+
     boolean delete(User user) throws SQLException {
         String sql = String.format(
                 "DELETE FROM `users` WHERE `users`.`id` = %d",
@@ -33,12 +34,12 @@ class UserCRUD {
         );
         try (Connection connection = Connect.getConnection();
 
-        Statement statement = connection.createStatement()){
+             Statement statement = connection.createStatement()) {
             return (1 == statement.executeUpdate(sql));
         }
     }
 
-    User update(User user) throws  SQLException {
+    boolean update(User user) throws SQLException {
         String sql = String.format(
                 "UPDATE `users` SET " +
                         "`login` = '%s', `password` = '%s', " +
@@ -48,26 +49,29 @@ class UserCRUD {
                 user.getEmail(), user.getRoles_Id(),
                 user.getId()
         );
-
-        User read(long id) throw  SQLException {
-            String sql=String.format("SELECT `id`, `login`, `password`, `email`, `roles_id` " +
-                    "FROM `users` WHERE id=%d",id);
-            try (Connection connection= Connect.getConnection();
-            Statement statement = connection.createStatement()){
-                ResultSet resultSet = statement.executeQuery(sql);
-                if (resultSet.next()){
-                    // id ok
-                    String login=resultSet.getString("login");
-                    String password=resultSet.getString("password");
-                    String email=resultSet.getString("email");
-                    long roles_Id=resultSet.getLong("roles_Id");
-                    return new  User(id,login,password,email,roles_Id);
-                }
-                else
-                    return null;
-
-            }
+        try (Connection connection = Connect.getConnection();
+             Statement statement = connection.createStatement()) {
+            return (1 == statement.executeUpdate(sql));
         }
-
     }
 
+    User read(long id) throws SQLException {
+        String sql = String.format("SELECT `id`, `login`, `password`, `email`, `roles_id` " +
+                "FROM `users` WHERE id=%d", id);
+        try (Connection connection = Connect.getConnection();
+             Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(sql);
+            if (resultSet.next()) {
+                // id ok
+                String login = resultSet.getString("login");
+                String password = resultSet.getString("password");
+                String email = resultSet.getString("email");
+                long roles_Id = resultSet.getLong("roles_Id");
+                return new User(id, login, password, email, roles_Id);
+            } else
+                return null;
+
+        }
+    }
+
+}
