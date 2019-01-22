4509fb23a97d825cd7cfba37a00fc46b2dc788aa
jerome <drel@icloud.com>
2019-01-22 15:47:10 +0300

    jd03_03_start

--- src/by/it/zhivov/jd03_02/Runner.java
+++ src/by/it/zhivov/jd03_02/Runner.java
@@ -1,9 +1,11 @@
 package by.it.zhivov.jd03_02;
 
 import by.it.zhivov.jd03_02.beans.Ad;
+import by.it.zhivov.jd03_02.beans.Role;
 import by.it.zhivov.jd03_02.beans.User;
 import by.it.zhivov.jd03_02.crud.AdCRUD;
 import by.it.zhivov.jd03_02.crud.C_Reset;
+import by.it.zhivov.jd03_02.crud.RoleCRUD;
 import by.it.zhivov.jd03_02.crud.UserCRUD;
 
 import java.sql.SQLException;
@@ -11,17 +13,31 @@ import java.sql.SQLException;
 public class Runner {
     public static void main(String[] args) throws SQLException {
         {
-            C_Reset.restoreDB();
             C_Reset.resetAds();
             C_Reset.resetUsers();
-            //C_Reset.resetDB();
-            //C_Reset.restoreDB();
+            C_Reset.resetDB();
+            C_Reset.restoreDB();
         }
-
         UserCRUD userCRUD = new UserCRUD();
         AdCRUD adCRUD = new AdCRUD();
+        RoleCRUD roleCRUD = new RoleCRUD();
         User user = new User();
         Ad ad = new Ad();
+        Role role = new Role();
+
+        //манипуляции с role
+        //create
+        role.setRole("moderator");
+        if (roleCRUD.create(role))
+            System.out.println("\tCreate role successful\n" + role + "\n");
+        //update
+        role.setRole("superuser");
+        if (roleCRUD.update(role))
+            System.out.println("\tUpdate role successful\n" + role + "\n");
+        //read
+        role = roleCRUD.read(role.getId());
+        System.out.println("\tRead role successful\n" + role + "\n");
+
         //манипуляции с таблицей user
         //create
         user.setName("Genady");
@@ -37,8 +53,6 @@ public class Runner {
         user.setEmail("changed!_setEmail done");
         if (userCRUD.update(user))
             System.out.println("\tUpdate user successful\n" + user + "\n");
-        user.setTel("changed!_setTel done");
-        ;
         //read
         user = userCRUD.read(user.getId());
         System.out.println("\tRead user successful\n" + user + "\n");
@@ -68,12 +82,17 @@ public class Runner {
         //read
         ad = adCRUD.read(ad.getId());
         System.out.println("\tRead ad successful\n" + ad + "\n");
+        {
             //delete ad
             if (adCRUD.delete(ad))
                 System.out.println("\tDelete ad succefull\n" + ad + "\n");
             //delete user
             if (userCRUD.delete(user))
                 System.out.println("\tDelete user successful\n" + user + "\n");
+            //delete role
+            if (roleCRUD.delete(role))
+                System.out.println("\tDelete role successful\n" + user + "\n");
+        }
 
     }
 }
