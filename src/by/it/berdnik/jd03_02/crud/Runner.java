c2e64d1f6123e8c32f42cf61a4685ce8cc6c45ab
Yuriy Berdnik <yuriy8671@gmail.com>
2019-01-23 20:38:06 +0300

    jd03_02

--- src/by/it/berdnik/jd03_02/crud/Runner.java
+++ src/by/it/berdnik/jd03_02/crud/Runner.java
@@ -10,21 +10,21 @@ public class Runner {
     public static void main(String[] args) throws SQLException {
 
 
-        RoleCRUD roleCRUD = new RoleCRUD();
-        Role role = new Role();
-        role.setRole("UUUUUU");
-        if (roleCRUD.create(role))
-            System.out.println("CREATE: " + role);
-
-        role = roleCRUD.read(role.getId());
-        System.out.println("READ: " + role);
-
-        role.setRole("UUUUUU222");
-        if (roleCRUD.update(role))
-            System.out.println("UPDATE: " + role);
-
-        if (roleCRUD.delete(role))
-            System.out.println("DELETE: " + role);
+//        RoleCRUD roleCRUD = new RoleCRUD();
+//        Role role = new Role();
+//        role.setRole("UUUUUU");
+//        if (roleCRUD.create(role))
+//            System.out.println("CREATE: " + role);
+//
+//        role = roleCRUD.read(role.getId());
+//        System.out.println("READ: " + role);
+//
+//        role.setRole("UUUUUU222");
+//        if (roleCRUD.update(role))
+//            System.out.println("UPDATE: " + role);
+//
+//        if (roleCRUD.delete(role))
+//            System.out.println("DELETE: " + role);
 
 
 
@@ -49,23 +49,23 @@ public class Runner {
 
 
 
-        BuyerCRUD buyerCRUD = new BuyerCRUD();
-        Buyer buyer = new Buyer();
-        buyer.setItem("IIIIIII");
-        buyer.setSpecif("SSSSSSS");
-        buyer.setPrice(1234);
-        buyer.setAddress("STREET");
-        buyer.setUsers_Id(2);
-        if (buyerCRUD.create(buyer))
-            System.out.println("CREATE: " + buyer);
-
-                buyer = buyerCRUD.read(buyer.getId());
-        System.out.println("READ: " + buyer);
-
-        if (buyerCRUD.update(buyer))
-            System.out.println("UPDATE: " + buyer);
-
-        if (buyerCRUD.delete(buyer))
-            System.out.println("DELETE: " + buyer);
+//        BuyerCRUD buyerCRUD = new BuyerCRUD();
+//        Buyer buyer = new Buyer();
+//        buyer.setItem("IIIIIII");
+//        buyer.setSpecif("SSSSSSS");
+//        buyer.setPrice(1234);
+//        buyer.setAddress("STREET");
+//        buyer.setUsers_Id(2);
+//        if (buyerCRUD.create(buyer))
+//            System.out.println("CREATE: " + buyer);
+//
+//                buyer = buyerCRUD.read(buyer.getId());
+//        System.out.println("READ: " + buyer);
+//
+//        if (buyerCRUD.update(buyer))
+//            System.out.println("UPDATE: " + buyer);
+//
+//        if (buyerCRUD.delete(buyer))
+//            System.out.println("DELETE: " + buyer);
     }
 }
