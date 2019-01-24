875e5141740fff635db6339fac7493f85b26d3bf
Khmelov <375336849110@tut.by>
2019-01-24 12:33:07 +0300

    classwork 03_04

---
+++ src/by/it/akhmelev/project/java/controller/Action.java
@@ -0,0 +1,20 @@
+package by.it.akhmelev.project.java.controller;
+
+enum Action {
+
+    INDEX(new CmdIndex()),
+    LOGIN(new CmdLogin()),
+    LOGOUT(new CmdLogout()),
+    SIGNUP(new CmdSignup()),
+    ERROR(new CmdError());
+
+    Cmd cmd;
+
+    Action(Cmd cmdIndex) {
+        cmd = cmdIndex;
+    }
+
+    String getJsp() {
+        return "/" + cmd.toString() + ".jsp";
+    }
+}
