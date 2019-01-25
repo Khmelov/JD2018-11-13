94eeb85559807f785539208e4a73d5007ad3f2a3
Катя <katia.denisova.99@mail.ru>
2019-01-25 22:36:51 +0300

    fixed

--- src/by/it/denisova/project/java/controller/FrontController.java
+++ src/by/it/denisova/project/java/controller/FrontController.java
@@ -22,6 +22,7 @@ public class FrontController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader("Cache-Control", "no-store");
         process(req, resp);
     }
 
