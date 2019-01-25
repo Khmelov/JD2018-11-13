e34505e679f7c780c6b4074861e1b49bafb88dfa
MarkelaLippi <MarkelaLippi@gmail.com>
2019-01-25 18:46:22 +0300

    jd03_04

---
+++ src/by/it/markelov/jd03_04/java/controller/CmdSignup.java
@@ -0,0 +1,10 @@
+package by.it.markelov.jd03_04.java.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+class CmdSignup extends Cmd{
+    @Override
+    Action execute(HttpServletRequest req) {
+        return null;
+    }
+}
