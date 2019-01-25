a80befc65ab3b5e99076421ec524ee5c9f343499
maksimbaidak <maksimbaidk@gmail.com>
2019-01-25 03:49:34 +0300

    Role + User + Post DAO

---
+++ src/by/it/baidak/jd03_03/beans/Post.java
@@ -0,0 +1,61 @@
+package by.it.baidak.jd03_03.beans;
+
+public class Post {
+
+    private long id;
+    private long userID;
+    private String head;
+    private String date;
+
+    public Post() {
+    }
+
+    public Post(long id, long userID, String head, String date) {
+        this.id = id;
+        this.userID = userID;
+        this.head = head;
+        this.date = date;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(long userID) {
+        this.userID = userID;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public void setHead(String head) {
+        this.head = head;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", userID=" + userID +
+                ", head='" + head + '\'' +
+                ", date='" + date + '\'' +
+                '}';
+    }
+}
