064f0d64546fc1cb9d5d6d02e38d031d7be61c6c
Renat Serahlazau <serahlazau@outlook.com>
2019-01-26 00:33:32 +0300

    jd03_04

---
+++ src/by/it/seroglazov/project/java/dao/Dao.java
@@ -0,0 +1,30 @@
+package by.it.seroglazov.project.java.dao;
+
+import java.util.List;
+
+public interface Dao<T> {
+
+    boolean create(T t) throws Exception;
+
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
+    boolean update(T t) throws Exception;
+
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
+    boolean delete(T t) throws Exception;
+
+    T read(long id) throws Exception;
+
+    List<T> getAll(String sqlCondition) throws Exception;
+
+    default List<T> getAll() throws Exception {
+        return getAll("");
+    }
+
+    default T findFirstByFieldValue(String pole, String value) throws Exception {
+        String sqlSuffix = String.format("WHERE `%s` = '%s'", pole, value);
+        List<T> list = getAll(sqlSuffix);
+        if (list.size() > 0) return list.get(0);
+        else return null;
+    }
+
+}
