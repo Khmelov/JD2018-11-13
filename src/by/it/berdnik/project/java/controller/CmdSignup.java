2ff954884ce230bded22e9cb34d8a8dca100a034
Yuriy Berdnik <yuriy8671@gmail.com>
2019-01-26 15:40:04 +0300

    project

--- src/by/it/berdnik/project/java/controller/CmdSignup.java
+++ src/by/it/berdnik/project/java/controller/CmdSignup.java
@@ -1,16 +1,24 @@
 package by.it.berdnik.project.java.controller;
 
+import by.it.berdnik.project.java.beans.User;
+
 import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLException;
 
 class CmdSignup extends Cmd {
     @Override
     Action execute(HttpServletRequest req) {
-        String login = req.getParameter("login");
-        String password = req.getParameter("password");
-        String email = req.getParameter("email");
-//        User user = new User(0, login, password, email, 2);
-//        Dao dao = Dao.getDao();
+        if (req.getMethod().equalsIgnoreCase("POST")) {
 
-        return null;
+            String login = req.getParameter("login");
+            String password = req.getParameter("password");
+            String email = req.getParameter("email");
+            User user = new User(0, login, password, email, 2);
+//            Dao dao = Dao.getDao();
+//            if (dao.user.create(user))
+                return Action.INDEX;
+        }
+
+        return Action.SIGNUP;
     }
 }
