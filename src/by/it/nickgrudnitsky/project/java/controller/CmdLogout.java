c0db4f4ad46ab7a582708e8b7acf3627d6101fc3
Nick Grudnitsky <grud97@gmail.com>
2019-01-30 23:17:27 +0300

    Project Version 0.3.0

--- src/by/it/nickgrudnitsky/project/java/controller/CmdLogout.java
+++ src/by/it/nickgrudnitsky/project/java/controller/CmdLogout.java
@@ -5,18 +5,13 @@ package by.it.nickgrudnitsky.project.java.controller;
 
 
 import javax.servlet.http.HttpServletRequest;
-
+import javax.servlet.http.HttpServletResponse;
 
 
 class CmdLogout extends Cmd {
     @Override
-    Action execute(HttpServletRequest req) {
-        if (Form.isPost(req)){
-            /*if (Form.getString(req, "login")!=null){*/
+    Action execute(HttpServletRequest req, HttpServletResponse resp) {
                 req.getSession().invalidate();
                 return Action.LOGIN;
-            }
-        /*}*/
-        return Action.PROFILE;
     }
 }
