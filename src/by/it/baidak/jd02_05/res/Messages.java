8e8fe9d344700c7e955c3df57b120c80fe86f30e
maksimbaidak <maksimbaidk@gmail.com>
2019-02-03 15:16:46 +0300

    project

---
+++ src/by/it/baidak/jd02_05/res/Messages.java
@@ -0,0 +1,7 @@
+package by.it.baidak.jd02_05.res;
+
+public interface Messages {
+    String STR_WELCOME="str.welcome";
+    String STR_QUESTION="str.question";
+    String USER_NAME="user.name";
+}
