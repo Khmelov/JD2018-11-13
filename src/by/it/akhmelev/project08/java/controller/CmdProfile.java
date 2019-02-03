6b59cd4daac185c65f77e2968eb163907f519a09
Khmelov <akhmelev@gmail.com>
2019-02-03 13:14:27 +0300

    classwork jd03_08

---
+++ src/by/it/akhmelev/project08/java/controller/CmdProfile.java
@@ -0,0 +1,32 @@
+package by.it.akhmelev.project08.java.controller;
+
+
+import by.it.akhmelev.project08.java.beans.Ad;
+import by.it.akhmelev.project08.java.beans.User;
+import by.it.akhmelev.project08.java.dao.Dao;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLException;
+import java.util.List;
+
+class CmdProfile implements Cmd {
+    @Override
+    public Action execute(HttpServletRequest req) throws SQLException, SiteException {
+        if (!Util.checkUser(req))
+            return Action.LOGIN;
+
+        if (Form.isPost(req)) {
+            if (Form.getString(req, "logout") != null) {
+                req.getSession().invalidate();
+                return Action.LOGIN;
+            }
+        }
+        User user = Util.findUser(req);
+        String where=String.format(" WHERE `users_id`='%d'",user.getId());
+
+        List<Ad> ads = Dao.getDao().ad.getAll(where);
+        req.setAttribute("ads",ads);
+
+        return Action.PROFILE;
+    }
+}
