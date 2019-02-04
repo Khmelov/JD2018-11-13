0e2febd28ae3dd0ee239bb7cc15d259486ea489d
Катя <katia.denisova.99@mail.ru>
2019-02-04 21:36:06 +0300

    form (Edit Users) - works but not fiend ${users} in index.jsp before reset or login/sign up

--- src/by/it/denisova/project/java/beans/User.java
+++ src/by/it/denisova/project/java/beans/User.java
@@ -60,7 +60,7 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
     @Override
     public String toString() {
         return "User{" +
